a8521fca7f3a725d9a8dc67b440d0b19f2920758
tharuzzzz <tharushas429@gmail.com>
2025-03-01 16:05:11 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = ` *Do not share this code to anyone* ‼️`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/tharuzzzz/Jpg/refs/heads/main/20241129_134919.jpg",
               },
               caption: sid,
             });
