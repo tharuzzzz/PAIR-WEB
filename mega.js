4c151159766a4a9d6fabcc7dac996f3929316759
tharuzzzz <tharushas429@gmail.com>
2025-03-01 15:33:50 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter",
+  email: "lokulokko61@gmail.com",
   password: "Lokko21#$%",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
