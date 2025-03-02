6afeb837aba76d4da80634023219612a83f63452
tharuzzzz <tharushas429@gmail.com>
2025-03-02 17:39:14 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "tharuandayoutu723@gmail.com",
-  password: "Tharu21##$$%%",
+  email: "lokulokko61@gmail.com",
+  password: "Lokko21#$%",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
